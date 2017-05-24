5298d7ac38a1dabf2994ebc0f3279df00a9cdaf8
Dennis <dmuturia2922@gmail.com>
2017-05-24 07:20:46 +0300

    Version 2

--- game.py
+++ game.py
@@ -3,17 +3,19 @@
 This is a guessing game
 Author: Dennis Muturia
 version: 1.0.0
+This is an improvement version
 '''
 import random
-
+print "Please enter your name"
+name = input()
 entries = 0
 def printouts():
      true_val = random.randrange(0, 20)#guesses numbers from 0 to 20
      for i in range(0, 5):
           entries =+ 1
-          print"Please enter the value"
+          print"Please enter the value" + " "+ name
           my_value = int(input())#input your value
-          
+
           if my_value <= true_val:
                print"Your value is low"
                print"Retry"
@@ -30,9 +32,6 @@ def main():
      print "This is my guessing game"
      print "Enter the value your value"
      printouts()
-     
+
 if __name__ == '__main__':
      main()
-     
-          
-                    
