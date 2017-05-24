5cc18d62252a6cba3f5631e2a98a7c023998e16b
Dennis <dmuturia2922@gmail.com>
2017-05-24 06:57:31 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,38 @@
+#usr/bin/python
+'''
+This is a guessing game
+Author: Dennis Muturia
+version: 1.0.0
+'''
+import random
+
+entries = 0
+def printouts():
+     true_val = random.randrange(0, 20)#guesses numbers from 0 to 20
+     for i in range(0, 5):
+          entries =+ 1
+          print"Please enter the value"
+          my_value = int(input())#input your value
+          
+          if my_value <= true_val:
+               print"Your value is low"
+               print"Retry"
+          elif my_value >= true_val:
+               print"Your value is high"
+               print"Retry"
+          elif my_value == true_val:
+               print"Correct. You guessed it correct. You have entered"+" "+str(entries)
+               break
+          else:
+               print "Invalid input"
+def main():
+     #main function
+     print "This is my guessing game"
+     print "Enter the value your value"
+     printouts()
+     
+if __name__ == '__main__':
+     main()
+     
+          
+                    
